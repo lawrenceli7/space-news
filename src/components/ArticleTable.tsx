603499f4d9d8414509aeaa7204d8bed403106c53
Lawrence Li <lawrenceli3202@gmail.com>
2024-05-05 23:45:37 -0400

    Fix table overflow

--- src/components/ArticleTable.tsx
+++ src/components/ArticleTable.tsx
@@ -1,28 +1,50 @@
 import { Article } from "@/types/types";
 import { Table, Typography } from "antd";
+import React from "react";
+import styled from "styled-components";
+
+const StyledTable = styled(Table)`
+  .ant-table-cell {
+    white-space: nowrap;
+    overflow: hidden;
+    text-overflow: ellipsis;
+  }
+
+  @media screen and (max-width: 750px) {
+    .ant-table-cell {
+      white-space: normal;
+    }
+  }
+`;
+
 interface ArticleTableProps {
   articles: Article[];
   loading: boolean;
 }
 
-const ArticleTable: React.FC<ArticleTableProps> = ({ articles, loading, }) => {
-
+const ArticleTable: React.FC<ArticleTableProps> = ({ articles, loading }) => {
   const columns = [
     {
       title: "Title",
       dataIndex: "title",
       key: "title",
-      render: (text: string, record: Article) => <a href={record.url} target="_blank" rel="noopener noreferrer">{text}</a>,
+      render: (text: string, record: Article) => (
+        <a href={record.url} target="_blank" rel="noopener noreferrer">
+          {text}
+        </a>
+      ),
     },
     {
       title: "News Source",
       dataIndex: "news_site",
       key: "news_site",
+      ellipsis: true,
     },
     {
       title: "Published At",
       dataIndex: "published_at",
       key: "published_at",
+      ellipsis: true,
       render: (text: string) => {
         const options: Intl.DateTimeFormatOptions = {
           month: "long",
@@ -38,16 +60,16 @@ const ArticleTable: React.FC<ArticleTableProps> = ({ articles, loading, }) => {
   ];
 
   return (
-    <>
+    <div>
       <Typography.Title level={2}>Articles</Typography.Title>
-      <Table
+      <StyledTable
         dataSource={articles}
         columns={columns}
         loading={loading}
         pagination={false}
+        scroll={{ x: true }}
       />
-    </>
-
+    </div>
   );
 };
 
